d8fb2f0d971c9a1cbceaf366b092669c282199ec
kunal.zanke <kunal.zanke@globallogic.com>
2022-11-01 17:25:13 +0530

    added backend url

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ import {
   HttpLink,
 } from "@apollo/client";
 
-const httpLink = new HttpLink({ uri: "http://localhost:4000/graphql" });
+const httpLink = new HttpLink({ uri: process.env.BACKEND_URL });
 
 const authLink = new ApolloLink((operation, forward) => {
   const token = localStorage.getItem("token");
